b7bd9b60f5b1db5c0c3a28898a3b7d3ed4c2daab
d.agasyan <d.agasyan@getproduction.ru>
2023-01-17 13:42:45 +0400

    commit

---
+++ src/pages/[id]/index.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {useRouter} from "next/router";
+// import '../../styles/styles.scss'
+
+const Index = () => {
+
+    const [beer, setBeer] = useState<any>(null)
+    const router = useRouter()
+    useEffect(() => {
+        if (router.query.id) {
+            axios.get('https://api.punkapi.com/v2/beers/' + router.query.id).then(({data}) => {setBeer(data[0])})
+        }
+        console.log(router)
+    }, [])
+    if (!beer) {
+        return <div>
+            <h1>loading</h1>
+        </div>
+    }
+    return (
+        <div className='beer-cart'>
+            <h1><span>Beer name: </span>{beer.name}</h1>
+            <h2><span>Description: </span> {beer.description}</h2>
+            <h3><span>Tagline: </span>{beer.tagline}</h3>
+            <h3><span>Abv: </span> {beer.abv}</h3>
+            <h3><span>Food pairing: </span> {beer.food_pairing}</h3>
+            <div className='beer-image'>
+                <img src={beer.image_url}/>
+            </div>
+        </div>
+    );
+};
+
+export default Index;
